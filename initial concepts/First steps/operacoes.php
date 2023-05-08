69d6480bb5efa1e3e79814075f8afa52cfe74660
Matheus Henrique Ptasinski Rosa <113607124+matheushpr9@users.noreply.github.com>
2023-05-08 20:43:56 +0000

    Added files with examples of variables and operators in PHP.

---
+++ initial concepts/First steps/operacoes.php
@@ -0,0 +1,17 @@
+<?php
+
+$age = 22;
+$ageIn10Years = $age +10;
+$ageIn5Years = $ageIn10Years - 5;
+
+echo $ageIn10Years;
+echo "\n";
+echo $ageIn5Years;
+
+//inShort
+$sum = 2 + 2;
+$subtraction = 2 - 2;
+$multiplication = 2 * 2;
+$division = 2 / 2;
+$power = 2 ** 2;
+$restOfDivision = 2 % 2;
---
+++ initial concepts/First steps/variavel.php
@@ -0,0 +1,9 @@
+<?php
+
+$age = 22;
+
+echo $age;
+
+$age = 23;
+
+echo $age;
