90dd58f86ce886f1c00342d6ac333c6c8a59de9f
Matheus Henrique Ptasinski Rosa <113607124+matheushpr9@users.noreply.github.com>
2023-05-12 20:08:13 +0000

    incrementing the strings.php file, adding examples of special characters

--- initial concepts/First steps/strings.php
+++ initial concepts/First steps/strings.php
@@ -5,6 +5,13 @@ echo "Hello World!\n";
 $age = 22;
 echo "My age is " . $age .  "years old.\n\n";
 
-echo 'My age is $age years old.'."\n\n";
+echo 'My age is $age years old.'."\n\n";// remember, for php, everything between single quotes(' '), is represented only as a text
 //notice the difference in the output
 echo "My age is $age years old.\n";
+
+echo "\ttabulated string";//the "/t"tabulate a output
+
+//the special line skip character can change according to the operating system, so PHP created its own variable to represent it that works regardless of the operating system, it is stored in the variable PHP_EOL
+echo PHP_EOL . "Example" . PHP_EOL;
+
+//for more, read the documentation in: https://www.php.net/manual/en/language.types.string.php
