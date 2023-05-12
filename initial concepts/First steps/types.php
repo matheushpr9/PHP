24ac84dce6a57f645f74103951ee19709e14a349
Matheus Henrique Ptasinski Rosa <113607124+matheushpr9@users.noreply.github.com>
2023-05-12 19:36:57 +0000

    created the file with examples of variable types in PHP.

---
+++ initial concepts/First steps/types.php
@@ -0,0 +1,27 @@
+<?php
+
+$age = 22;
+$salary = 1400.50;
+$seconds = 12.3214431;
+$division = 10/3;
+
+echo gettype($age);
+echo "\n";
+
+echo gettype($salary);
+echo "\n";
+
+echo gettype($seconds);
+echo "\n";
+
+echo gettype($division);
+echo "\n";
+
+$name = "Joel";
+$comparison=  (1>2);
+
+echo gettype($name);
+echo "\n";
+
+echo gettype($comparison);
+echo "\n";
