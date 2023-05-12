e892fa408bed70105cb20bd8a2c3783dc415783b
Matheus Henrique Ptasinski Rosa <113607124+matheushpr9@users.noreply.github.com>
2023-05-12 19:50:42 +0000

    created the file that explains about the concatenation and interpolation of strings

---
+++ initial concepts/First steps/strings.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Hello World!\n";
+//obs: the "\n" skip the line
+$age = 22;
+echo "My age is " . $age .  "years old.\n\n";
+
+echo 'My age is $age years old.'."\n\n";
+//notice the difference in the output
+echo "My age is $age years old.\n";
