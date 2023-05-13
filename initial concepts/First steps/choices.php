0eb67501c2d1f4a7fbab789f9a351c2238fa8de1
Matheus Henrique Ptasinski Rosa <113607124+matheushpr9@users.noreply.github.com>
2023-05-13 13:46:15 +0000

    created the file choices.php, to explain if, else if and else, and boolean operators

---
+++ initial concepts/First steps/choices.php
@@ -0,0 +1,28 @@
+<?php
+
+$age = 16;
+$numberOfPeople = 1;
+
+echo "You can only enter if you are over 18 years old." . PHP_EOL;
+
+if($age > 18 || $age == 18){
+    echo "You can enter, welcome!";
+}else{
+    echo "You can't enter, get out!" . PHP_EOL;
+}
+echo PHP_EOL . "---------------------" . PHP_EOL;
+
+//or
+echo "You can only enter if you are over 18 years old." . PHP_EOL;
+echo "Or, 16 year, accompanied!" . PHP_EOL;
+
+if($age >= 18){
+    echo "You can enter, welcome!";
+}else if($age >=16 && $numberOfPeople >1){
+    echo "You have $age years,and is accompanied, Welcome!";
+}else{
+    echo "You can't enter, get out!";
+}
+
+
+// boolean operators in PHP (and &&) and (or ||) 
